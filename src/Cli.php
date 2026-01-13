1d8865935a17a86a2060a26c86621e1ff5629aa2
Max Kavun <max.kavun@gmail.com>
2026-01-13 12:55:23 +0300

    123

--- src/Cli.php
+++ src/Cli.php
@@ -6,11 +6,9 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcome(): string
+function welcome()
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line('Hello, %s!', $name);
-
-    return $name;
 }
